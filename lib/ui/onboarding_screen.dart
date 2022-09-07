9fe0e3d3ece7145e5fdae2a5ff8f173c00091f23
Lakshay Raturi <85173668+lakshayraturi@users.noreply.github.com>
2022-09-07 14:35:39 +0530

    adding new features

--- lib/ui/onboarding_screen.dart
+++ lib/ui/onboarding_screen.dart
@@ -16,7 +16,55 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('Onboarding Screen'),
+        elevation: 0,
+        backgroundColor: Colors.white,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20, top: 20),
+            child: InkWell(
+              onTap: () => null, //to login screen. We will update later
+              child: const Text(
+                'Skip',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          PageView(
+            onPageChanged: (int page) {
+              setState(() {
+                currentIndex = page;
+              });
+            },
+            controller: _pageController,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(
+                  left: 50,
+                  right: 50,
+                  bottom: 80,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 350,
+                      child: Image.asset('assets/images/plant-one.png'),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
